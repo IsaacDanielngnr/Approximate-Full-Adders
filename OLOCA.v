5e7499fafe6b863efe05999683c7d52fe732122f
IsaacDanielngnr <112849587+IsaacDanielngnr@users.noreply.github.com>
2022-09-06 15:22:03 +0530

    Update OLOCA.v

--- OLOCA.v
+++ OLOCA.v
@@ -1,41 +1,76 @@
-`timescale 1ns / 1ps
-module test;
+///////////////////////////////////////////////////////////PRECISE ADDER
+module precise_adder(input1,input2,answer,carry_out);
+	parameter UPL=4;
+	input [UPL-1:0] input1,input2;
+	output [UPL-1:0] answer;
+	output  carry_out;
+	wire [UPL-1:0] carry;
+	
+	genvar i;
+	generate 
+		for(i=0;i<UPL;i=i+1) begin: generate_N_bit_Adder
+			if(i==0) 
+				half_adder f(input1[0],input2[0],answer[0],carry[0]);
+			else
+				full_adder f(input1[i],input2[i],carry[i-1],answer[i],carry[i]);
+		end
   
-  parameter N=16;
-  reg [N-1:0] A,B;
-  wire [N:0] result;
-  wire [N:0] Esum;
-  bit signed [7:0] ED;
-  
-  top dut(A,B,result);
-  
-  	assign Esum = A + B;
- 	assign ED = Esum - result;
-  
-  initial begin
-
-        A = 'd2; B = 'd1;
-    
-    $dumpvars(0);
-    $dumpfile("dump.vcd");
-		
-    $display("..A..\t\t..B..\t\t..Esum..\t\t..Asum..\t\t..ED..");
-    $monitor("%d \t\t %d \t\t %d \t\t\t %d \t\t\t %d",A,B,Esum,result,ED,);
-		
-    #50
-    $finish;
-    
-    
-
-  end
-  
-  initial begin
-    forever #1 begin
-		
-      A = $urandom_range('d0,'d65535);
-      B = $urandom_range('d0,'d65535);
-     
-	end
-  end
-
+		assign carry_out = carry[UPL-1];
+	endgenerate
+	
 endmodule
+
+module half_adder(x,y,s,c);
+   input x,y;
+   output s,c;
+   assign s=x^y;
+   assign c=x&y;
+endmodule // half adder
+
+module full_adder(x,y,c_in,s,c_out);
+   input x,y,c_in;
+   output s,c_out;
+	assign s = (x^y) ^ c_in;
+	assign c_out = (y&c_in)| (x&y) | (x&c_in);
+endmodule // full_adder
+
+///////////////////////////////////////////////////////////////////IMPRECISE ADDER
+module imprecise_adder(A,B,result);
+	parameter LPL=4;
+	input [LPL - 1:0] A,B;
+	output [LPL - 1:0] result;
+	
+	assign result[LPL-1] = A[LPL-1] | B[LPL-1];
+	assign result[LPL-2] = A[LPL-2] | B[LPL-2];
+	assign result[LPL-3:0] = 1'b1;
+endmodule : imprecise_adder
+
+///////////////////////////////////////////////////////////////////top
+module top(A,B,result);
+	parameter N=8;
+	parameter LPL=4;
+	parameter UPL=4;
+	input [N - 1:0] A,B;
+	output [N:0] result;
+	
+	wire [LPL-1:0] A_lsb;
+	wire [LPL-1:0] B_lsb;
+	wire [LPL-1:0] sum_lsb;
+	wire [UPL-1:0] A_msb;
+	wire [UPL-1:0] B_msb;
+	wire [UPL-1:0] sum_msb;
+	wire carry_msb;
+	
+	imprecise_adder lsb(.A(A_lsb),.B(B_lsb),.result(sum_lsb));
+	precise_adder msb(.input1(A_msb),.input2(B_msb),.answer(sum_msb),.carry_out(carry_msb));
+	
+	assign A_lsb = A[LPL-1:0];
+	assign B_lsb = B[LPL-1:0];
+	assign result[LPL-1:0] = sum_lsb;
+	assign A_msb = A[N-1:LPL];
+	assign B_msb = B[N-1:LPL];
+	assign result[N-1:LPL] = sum_msb;
+	assign result[N] = carry_msb;
+	
+	
+endmodule : top
